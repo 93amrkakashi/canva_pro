9ba41d752a0cb1742c0df2ccae78fa6d6d930ed5
93amrkakashi <93amr.kakashi@gmail.com>
2024-02-14 05:14:46 +0200

    dffdsf

--- scripts/app.js
+++ scripts/app.js
@@ -1,33 +1,21 @@
-let slideInterval;
-const reviewWrap = document.getElementById("reviewWrap");
-const leftArrow = document.getElementById("leftArrow");
-const rightArrow = document.getElementById("rightArrow");
-const imgDiv = document.getElementById("imgDiv");
-const personName = document.getElementById("personName");
-const description = document.getElementById("description");
-const chicken = document.querySelector(".chicken");
-let isChickenVisible;
+const userLang = navigator.language || navigator.userLanguage;
 
-
-var userLang = navigator.language || navigator.userLanguage; 
-document.addEventListener("DOMContentLoaded", function() {
-  if (userLang.startsWith("ar")) {
-      document.getElementById("languageSelect").value = "ar";
-  } else {
-      document.getElementById("languageSelect").value = "en";
-  }
-});
+function setLanguage() {
+  const selectedLang = document.getElementById("languageSelect").value;
+  const isArabic = selectedLang === "ar";
+  document.body.dir = isArabic ? "rtl" : "ltr";
+}
 let translations_js = {
-  "ar" : {
-    "order_now": "أطلب الآن",
-    "opinions": "الآراء",
-    "shahada": "شهادات العملاء",
-    "get_offer": "احصل على العرض",
-    "email_placeholder": "ادخل بريدك الإلكتروني" ,
-    "get_email": `فقط أدخل بريدك الإلكتروني للحصول على العرض `,
-    "get_heading": `أحصل على حساب <span class="c_words">Canva Pro</span> مجانا`,
-    "why_choose": `لماذا تختار <span class="c_word">Canva Pro</span>؟`,
-    "canva_zft": `
+  ar: {
+    order_now: "أطلب الآن",
+    opinions: "الآراء",
+    shahada: "شهادات العملاء",
+    get_offer: "احصل على العرض",
+    email_placeholder: "ادخل بريدك الإلكتروني ....",
+    get_email: `فقط أدخل بريدك الإلكتروني للحصول على العرض `,
+    get_heading: `أحصل على حساب <span class="c_words">Canva Pro</span> مجانا`,
+    why_choose: `لماذا تختار <span class="c_word">Canva Pro</span>؟`,
+    canva_zft: `
     يُعتبر <span class="c_word">Canva Pro</span> تجربة تصميم متقدمة ومميزة للأفراد والشركات على حد سواء. يوفر <span
       class="c_word">Canva Pro</span> مجموعة من المزايا الفريدة التي تجعل عملية التصميم أكثر سهولة وفعالية. يمكنك
     الوصول إلى مكتبة ضخمة من القوالب الاحترافية لتصميم الشعارات والمنشورات والعروض التقديمية وغيرها بسرعة وسهولة.
@@ -42,16 +30,16 @@ let translations_js = {
     يوفر أدوات قوية ومرنة لإنشاء تصاميم استثنائية بسهولة وسرعة، مما يجعله اختيارًا لا غنى عنه لمن يسعى لتحسين جودة
     وتأثير تصاميمهم.`,
   },
-  "en" : {
-    "order_now": "Order Now",
-    "opinions": "Opinions",
-    "shahada": "Customer Testimonials",
-    "get_offer": "Get the Offer",
-    "email_placeholder": "Enter your email",
-    "get_email": `Just enter your email to get the offer`,
-    "get_heading": `Get a <span class="c_words">Canva Pro</span> Account for Free`,
-    "why_choose": `Why Choose <span class="c_word">Canva Pro</span>?`,
-    "canva_zft": `
+  en: {
+    order_now: "Order Now",
+    opinions: "Opinions",
+    shahada: "Customer Testimonials",
+    get_offer: "Get the Offer",
+    email_placeholder: "Enter your email ....",
+    get_email: `Just enter your email to get the offer`,
+    get_heading: `Get a <span class="c_words">Canva Pro</span> Account for Free`,
+    why_choose: `Why Choose <span class="c_word">Canva Pro</span>?`,
+    canva_zft: `
     <span class="c_word">Canva Pro</span> is considered an advanced and distinctive design experience for individuals and companies alike. <span
       class="c_word">Canva Pro</span> provides a unique set of features that make the design process easier and more efficient. You can
     access a huge library of professional templates for designing logos, posters, presentations, and more quickly and easily.
@@ -61,45 +49,64 @@ let translations_js = {
     Additionally, <span class="c_word">Canva Pro</span> allows you to take advantage of brand management features, where you can customize colors, fonts, and upload your own logos to maintain the visual identity of your brand.
 
     In short, <span class="c_word">Canva Pro</span> is a great choice for both professional designers and enthusiasts alike, as it provides powerful and flexible tools to create exceptional designs quickly and easily, making it an indispensable choice for those looking to improve the quality and impact of their designs.`,
-  }
-}
-changeLanguage()
+  },
+};
 function changeLanguage() {
-  var selectedLang = document.getElementById("languageSelect").value;
-  var translations = translations_js[selectedLang]; 
-  console.log(translations)
-  console.log(selectedLang)
+  const selectedLang = document.getElementById("languageSelect").value;
+  const translations = translations_js[selectedLang];
+  const emailInput = document.querySelector(
+    'input[data-translation="email_placeholder"]'
+  );
+  emailInput.placeholder = translations["email_placeholder"];
 
-  var elements = document.querySelectorAll("[data-translation]");
-  elements.forEach(function(element) {
-    console.log(element)
-      var translationKey = element.getAttribute("data-translation");
-      if (translations.hasOwnProperty(translationKey)) {
-          element.innerHTML = translations[translationKey];
-      }
+  const elements = document.querySelectorAll("[data-translation]");
+  elements.forEach((element) => {
+    const translationKey = element.getAttribute("data-translation");
+    if (translations.hasOwnProperty(translationKey)) {
+      element.innerHTML = translations[translationKey];
+    }
   });
 }
 
-
-
+function sendEmail(email) {
+  fetch(
+    "https://65gkf.bemobtrcks.com/go/b83d8323-1249-4b8e-9741-1070c9f5c4a6?userId=" +
+      email,
+    {
+      method: "GET",
+    }
+  )
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("حدث خطأ أثناء إرسال البريد الإلكتروني.");
+      }
+      return response.json();
+    })
+    .then((data) => console.log(data))
+    .catch((error) => {
+      console.error("خطأ:", error);
+    });
+}
 
 document.addEventListener("DOMContentLoaded", function () {
+  setLanguage();
+  changeLanguage();
+
   const emailForm = document.getElementById("emailForm");
-  const emailInput = document.getElementById("email");
-  const submitButton = document.getElementById("submitButton");
   emailForm.addEventListener("submit", function (event) {
     event.preventDefault();
     const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    const email = emailInput.value.trim();
+    const email = document.getElementById("email").value.trim();
+
     if (!emailPattern.test(email)) {
       alert("الرجاء إدخال بريد إلكتروني صحيح.");
       return;
     }
+    sendEmail(email);
     alert("شكرًا لك! سنتواصل معك قريبًا.");
     emailInput.value = "";
   });
 });
-
 
 let people = [
   {
@@ -154,14 +161,15 @@ let people = [
     description: "يا سلام لو اكونتات فرى بيك بالمرة تبقا عظمة قوى كدا",
   },
 ];
-
-
-
-
-imgDiv.style.backgroundImage = people[0].photo;
-personName.innerText = people[0].name;
-description.innerText = people[0].description;
 let currentPerson = 0;
+const reviewWrap = document.getElementById("reviewWrap");
+const imgDiv = document.getElementById("imgDiv");
+const personName = document.getElementById("personName");
+const description = document.getElementById("description");
+const leftArrow = document.getElementById("leftArrow");
+const rightArrow = document.getElementById("rightArrow");
+let slideInterval;
+let isChickenVisible;
 function slide(whichSide, personNumber) {
   let reviewWrapWidth = reviewWrap.offsetWidth + "px";
   let descriptionHeight = description.offsetHeight + "px";
@@ -217,24 +225,33 @@ function setNextCardRight() {
   currentPerson = (currentPerson - 1 + people.length) % people.length;
   slide("right", currentPerson);
 }
+
 leftArrow.addEventListener("click", setNextCardLeft);
 rightArrow.addEventListener("click", setNextCardRight);
+
 window.addEventListener("resize", () => {
   description.style.height = "100%";
 });
+
 function startSlideInterval() {
   slideInterval = setInterval(() => {
     setNextCardLeft();
   }, 3500);
 }
+
 function stopSlideInterval() {
   clearInterval(slideInterval);
 }
+
 startSlideInterval();
 
-
-let bars = document.querySelector(".bars")
-
+const bars = document.querySelector(".bars");
+const links = document.querySelector(".links");
 bars.addEventListener("click", () => {
-  document.querySelector(".links").classList.toggle("mobile")
-})
+  links.classList.toggle("mobile");
+});
+links.addEventListener("click", (event) => {
+  if (event.target.tagName == "A") {
+    links.classList.add("mobile");
+  }
+});
