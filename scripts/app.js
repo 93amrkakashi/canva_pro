4a767b7cc31ca6ab95614df23c5619d00440cb3c
93amrkakashi <93amr.kakashi@gmail.com>
2024-02-14 04:29:20 +0200

    dsfd

--- scripts/app.js
+++ scripts/app.js
@@ -8,6 +8,81 @@ const description = document.getElementById("description");
 const chicken = document.querySelector(".chicken");
 let isChickenVisible;
 
+
+var userLang = navigator.language || navigator.userLanguage; 
+document.addEventListener("DOMContentLoaded", function() {
+  if (userLang.startsWith("ar")) {
+      document.getElementById("languageSelect").value = "ar";
+  } else {
+      document.getElementById("languageSelect").value = "en";
+  }
+});
+let translations_js = {
+  "ar" : {
+    "order_now": "أطلب الآن",
+    "opinions": "الآراء",
+    "shahada": "شهادات العملاء",
+    "get_offer": "احصل على العرض",
+    "email_placeholder": "ادخل بريدك الإلكتروني" ,
+    "get_email": `فقط أدخل بريدك الإلكتروني للحصول على العرض `,
+    "get_heading": `أحصل على حساب <span class="c_words">Canva Pro</span> مجانا`,
+    "why_choose": `لماذا تختار <span class="c_word">Canva Pro</span>؟`,
+    "canva_zft": `
+    يُعتبر <span class="c_word">Canva Pro</span> تجربة تصميم متقدمة ومميزة للأفراد والشركات على حد سواء. يوفر <span
+      class="c_word">Canva Pro</span> مجموعة من المزايا الفريدة التي تجعل عملية التصميم أكثر سهولة وفعالية. يمكنك
+    الوصول إلى مكتبة ضخمة من القوالب الاحترافية لتصميم الشعارات والمنشورات والعروض التقديمية وغيرها بسرعة وسهولة.
+
+    مع <span class="c_word">Canva Pro</span>، تحصل على وصول غير محدود إلى مكتبة الصور والرموز التعبيرية والرسوم
+    البيانية، مما يتيح لك إضافة لمسة فنية إلى تصاميمك بكل سهولة.
+
+    بالإضافة إلى ذلك، يتيح لك <span class="c_word">Canva Pro</span> الاستفادة من ميزات إدارة العلامات التجارية، حيث
+    يمكنك تخصيص الألوان والخطوط وتحميل الشعارات الخاصة بك للحفاظ على الهوية البصرية لعلامتك التجارية.
+
+    باختصار، يعتبر <span class="c_word">Canva Pro</span> خيارًا رائعًا للمصممين المحترفين والهواة على حد سواء، حيث
+    يوفر أدوات قوية ومرنة لإنشاء تصاميم استثنائية بسهولة وسرعة، مما يجعله اختيارًا لا غنى عنه لمن يسعى لتحسين جودة
+    وتأثير تصاميمهم.`,
+  },
+  "en" : {
+    "order_now": "Order Now",
+    "opinions": "Opinions",
+    "shahada": "Customer Testimonials",
+    "get_offer": "Get the Offer",
+    "email_placeholder": "Enter your email",
+    "get_email": `Just enter your email to get the offer`,
+    "get_heading": `Get a <span class="c_words">Canva Pro</span> Account for Free`,
+    "why_choose": `Why Choose <span class="c_word">Canva Pro</span>?`,
+    "canva_zft": `
+    <span class="c_word">Canva Pro</span> is considered an advanced and distinctive design experience for individuals and companies alike. <span
+      class="c_word">Canva Pro</span> provides a unique set of features that make the design process easier and more efficient. You can
+    access a huge library of professional templates for designing logos, posters, presentations, and more quickly and easily.
+
+    With <span class="c_word">Canva Pro</span>, you get unlimited access to a library of images, icons, and graphics, allowing you to add an artistic touch to your designs with ease.
+
+    Additionally, <span class="c_word">Canva Pro</span> allows you to take advantage of brand management features, where you can customize colors, fonts, and upload your own logos to maintain the visual identity of your brand.
+
+    In short, <span class="c_word">Canva Pro</span> is a great choice for both professional designers and enthusiasts alike, as it provides powerful and flexible tools to create exceptional designs quickly and easily, making it an indispensable choice for those looking to improve the quality and impact of their designs.`,
+  }
+}
+changeLanguage()
+function changeLanguage() {
+  var selectedLang = document.getElementById("languageSelect").value;
+  var translations = translations_js[selectedLang]; 
+  console.log(translations)
+  console.log(selectedLang)
+
+  var elements = document.querySelectorAll("[data-translation]");
+  elements.forEach(function(element) {
+    console.log(element)
+      var translationKey = element.getAttribute("data-translation");
+      if (translations.hasOwnProperty(translationKey)) {
+          element.textContent = translations[translationKey];
+      }
+  });
+}
+
+
+
+
 document.addEventListener("DOMContentLoaded", function () {
   const emailForm = document.getElementById("emailForm");
   const emailInput = document.getElementById("email");
@@ -158,38 +233,6 @@ function stopSlideInterval() {
 startSlideInterval();
 
 
-// to change language ==> getting translations and change page content
-function changeLanguage(selectElement) {
-  const selectedValue =
-    selectElement.value || localStorage.getItem("lang") || "ar";
-  document.documentElement.lang = selectedValue;
-  localStorage.setItem("lang", selectedValue);
-  const elements = document.querySelectorAll("[data-translation]");
-  elements.forEach((element) => {
-    const translationKey = element.dataset.translation;
-    element.textContent = translations_js[selectedValue][translationKey] || "";
-  });
-}
-
-// *************************************************
-// *************************************************
-// *************************************************
-// to load translations file
-async function loadTranslations() {
-  let lang = localStorage.getItem("lang") || "ar"
-  changeLanguage(lang);
-  // try {
-  //   const response = await fetch("./translations.json");
-  //   if (!response.ok) {
-  //     throw new Error(`Failed to load translations: ${response.status}`);
-  //   }
-  //   translations = await response.json();
-  
-  //   console.log(translations);
-  // } catch (error) {
-  //   console.error("Error loading translations:", error);
-  // }
-}
 let bars = document.querySelector(".bars")
 
 bars.addEventListener("click", () => {
